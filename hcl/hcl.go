b86cbc6ece56f5a9a1a59540a81db48f6a794fee
David Gamba <davidgamba@gmail.com>
2020-08-06 00:48:49 -0600

    Refactor hcl code to allow referencing other elements

--- hcl/hcl.go
+++ hcl/hcl.go
@@ -20,6 +20,7 @@ import (
 	"github.com/imdario/mergo"
 	"github.com/zclconf/go-cty/cty"
 	"github.com/zclconf/go-cty/cty/function"
+	"github.com/zclconf/go-cty/cty/gocty"
 )
 
 // Map -
@@ -27,6 +28,23 @@ type Map struct {
 	Size       *Size        `hcl:"size,block"`
 	Nodes      []*Node      `hcl:"node,block"`
 	Connectors []*Connector `hcl:"connector,block"`
+}
+
+var mapSchema = &hcl.BodySchema{
+	Blocks: []hcl.BlockHeaderSchema{
+		{Type: "size"},
+		{Type: "node", LabelNames: []string{"id"}},
+		{Type: "connector"},
+	},
+}
+
+var mapDefaults = Map{
+	Size: &Size{
+		Width:    1280,
+		Height:   768,
+		Margin:   40,
+		FontSize: 12,
+	},
 }
 
 type Size struct {
@@ -40,22 +58,27 @@ var sizeDefaults = Size{
 	Width:    1280,
 	Height:   768,
 	Margin:   40,
-	FontSize: 9,
+	FontSize: 12,
 }
 
 // Node -
 type Node struct {
-	ID          string `hcl:"label,label"`
+	ID          string `hcl:"id,label"`
 	Label       string `hcl:"label"`
 	Description string `hcl:"description,optional"`
 	X           int
 	Y           int
-	Visibility  int    `hcl:"visibility"`
+	Visibility  int    `hcl:"visibility" cty:"visibility"`
 	Evolution   string `hcl:"evolution"`
-	EvolutionX  int    `hcl:"x"`
+	EvolutionX  int    `hcl:"x" cty:"x"`
 	Fill        string `hcl:"fill,optional"`
 	Color       string `hcl:"color,optional"`
 }
+
+var nodeType = cty.Object(map[string]cty.Type{
+	"x":          cty.Number,
+	"visibility": cty.Number,
+})
 
 var nodeDefaults = Node{
 	Fill:  "white",
@@ -78,19 +101,10 @@ var connectorDefaults = Connector{
 	Type:  "normal",
 }
 
-var mapDefaults = Map{
-	Size: &Size{
-		Width:    1280,
-		Height:   768,
-		Margin:   40,
-		FontSize: 12,
-	},
-}
-
 func ParseHCL(w io.Writer, data []byte, filename string) (*hclparse.Parser, *hcl.File, error) {
 	parser := hclparse.NewParser()
 	f, diags := parser.ParseHCL(data, filename)
-	err := handleDiags(parser, diags, w)
+	err := handleDiags(w, parser, diags)
 	if err != nil {
 		return parser, f, fmt.Errorf("failure during input configuration parsing")
 	}
@@ -100,14 +114,14 @@ func ParseHCL(w io.Writer, data []byte, filename string) (*hclparse.Parser, *hcl
 func ParseHCLFile(w io.Writer, filename string) (*hclparse.Parser, *hcl.File, error) {
 	parser := hclparse.NewParser()
 	f, diags := parser.ParseHCLFile(filename)
-	err := handleDiags(parser, diags, w)
+	err := handleDiags(w, parser, diags)
 	if err != nil {
 		return parser, f, fmt.Errorf("failure during input configuration parsing")
 	}
 	return parser, f, nil
 }
 
-func handleDiags(parser *hclparse.Parser, diags hcl.Diagnostics, w io.Writer) error {
+func handleDiags(w io.Writer, parser *hclparse.Parser, diags hcl.Diagnostics) error {
 	if diags.HasErrors() {
 		wr := hcl.NewDiagnosticTextWriter(
 			w,              // writer to send messages to
@@ -122,31 +136,87 @@ func handleDiags(parser *hclparse.Parser, diags hcl.Diagnostics, w io.Writer) er
 }
 
 func DecodeMap(w io.Writer, parser *hclparse.Parser, f *hcl.File) (*Map, error) {
-	var mapDetails Map
+	mapDetails := &Map{}
+
+	content, diags := f.Body.Content(mapSchema)
+	err := handleDiags(w, parser, diags)
+	if err != nil {
+		return nil, err
+	}
 
 	ctx := &hcl.EvalContext{
 		Variables: map[string]cty.Value{},
 		Functions: map[string]function.Function{},
 	}
-	// TODO: Still using gohcl instead of hcldec
-	// See: https://github.com/zclconf/go-cty/issues/38
-	diags := gohcl.DecodeBody(f.Body, ctx, &mapDetails)
-	err := handleDiags(parser, diags, w)
-	if err != nil {
-		return nil, fmt.Errorf("failure during decoding")
+
+	for _, block := range content.Blocks {
+		switch block.Type {
+		case "size":
+			var size Size
+			diags := gohcl.DecodeBody(block.Body, ctx, &size)
+			err = handleDiags(w, parser, diags)
+			if err != nil {
+				return mapDetails, err
+			}
+			mapDetails.Size = &size
+		case "node":
+			var node Node
+			diags := gohcl.DecodeBody(block.Body, ctx, &node)
+			err = handleDiags(w, parser, diags)
+			if err != nil {
+				return mapDetails, err
+			}
+			node.ID = block.Labels[0]
+			mapDetails.Nodes = append(mapDetails.Nodes, &node)
+
+			v, err := gocty.ToCtyValue(node, nodeType)
+			if err != nil {
+				return mapDetails, err
+			}
+			// fmt.Printf("v: %#v\n", v)
+
+			var m map[string]cty.Value
+			n, ok := ctx.Variables["node"]
+			if !ok {
+				m = map[string]cty.Value{
+					block.Labels[0]: v,
+				}
+			} else {
+				// fmt.Printf("n: %#v\n", n)
+				// fmt.Printf("n: %#v\n", n.AsValueMap())
+				m = n.AsValueMap()
+				// fmt.Printf("m: %#v\n", m)
+				m[block.Labels[0]] = v
+			}
+			// fmt.Printf("m: %#v\n", m)
+			ctx.Variables["node"] = cty.MapVal(m)
+			// fmt.Printf("ctx: %#v\n", ctx)
+		case "connector":
+			var connector Connector
+			diags := gohcl.DecodeBody(block.Body, ctx, &connector)
+			err = handleDiags(w, parser, diags)
+			if err != nil {
+				return mapDetails, err
+			}
+			mapDetails.Connectors = append(mapDefaults.Connectors, &connector)
+		}
 	}
 
-	err = mergo.Merge(&mapDetails, mapDefaults)
-	if err != nil {
-		return nil, fmt.Errorf("failure calculating map defaults: %w", err)
-	}
+	// TODO: New way to calculate defaults
+	// err = mergo.Merge(&mapDetails, mapDefaults)
+	// if err != nil {
+	// 	return nil, fmt.Errorf("failure calculating map defaults: %w", err)
+	// }
 
 	// TODO: Figure why this fails
-	// size := mapDetails.Size
-	// err := mergo.Merge(size, sizeDefaults)
-	// if err != nil {
-	// 	return nil, fmt.Errorf("failure calculating size defaults: %w", err)
-	// }
+	if mapDetails.Size == nil {
+		mapDetails.Size = &sizeDefaults
+	}
+	size := mapDetails.Size
+	err = mergo.Merge(size, sizeDefaults)
+	if err != nil {
+		return nil, fmt.Errorf("failure calculating size defaults: %w", err)
+	}
 
 	for _, node := range mapDetails.Nodes {
 		err := mergo.Merge(node, nodeDefaults)
@@ -160,5 +230,5 @@ func DecodeMap(w io.Writer, parser *hclparse.Parser, f *hcl.File) (*Map, error) 
 			return nil, fmt.Errorf("failure calculating connector defaults: %w", err)
 		}
 	}
-	return &mapDetails, nil
+	return mapDetails, nil
 }
